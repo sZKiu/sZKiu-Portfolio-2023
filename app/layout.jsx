1bea841644f648f01844d32dc5051354cee1f442
sZKiu <augustomendez544@gmail.com>
2023-01-09 19:04:25 -0300

    Problem with header probably solved-2

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,21 +1,11 @@
 import "../styles/globals.css";
-import { ContextProvider } from "../context/StaticContext";
-import { ThemeContextProvider } from "../context/ThemeContext";
 import Head from "./head";
-import Header from "../components/Home/Generic/Header";
 
 export default function RootLayout({ children }) {
   return (
     <html>
       <Head />
-      <body className="xl:flex">
-        <ContextProvider>
-          <ThemeContextProvider>
-            <Header />
-            {children}
-          </ThemeContextProvider>
-        </ContextProvider>
-      </body>
+      <body className="xl:flex">{children}</body>
     </html>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,19 @@
 "use client";
 import ShowPage from "../components/Home/Generic/ShowPage/ShowPage";
+import { ContextProvider } from "../context/StaticContext";
+import { ThemeContextProvider } from "../context/ThemeContext";
+import Header from "../components/Home/Generic/Header";
 
 const Home = () => {
-  return <ShowPage />;
+  return (
+    <ContextProvider>
+      <ThemeContextProvider>
+        <Header />
+        
+        <ShowPage />
+      </ThemeContextProvider>
+    </ContextProvider>
+  );
 };
 
 export default Home;
